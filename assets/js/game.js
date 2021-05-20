fec566934b1092c4d354cb356ae6ca767fe21478
ShiftyKitty <cathalpmcgahan@gmail.com>
2021-05-20 22:20:52 +0000

    icons added to tile-front and centered. transition effect also added to show tiles coming out of timer upon reset

---
+++ assets/js/game.js
@@ -0,0 +1,47 @@
+//ensure tiles have clicked functionality in place
+let tile = document.getElementsByClassName("tile");
+
+
+
+
+
+
+//shuffles all tiles on board on game start and game reset
+function shuffleTiles () {
+
+}
+
+//ensures correctly selected tiles are matched correctly
+//incorrect tile match leads to selected tiles changing places
+function tileMatch () {
+
+}
+
+//starts the game
+//all tiles shuffled
+//by starting game all tiles appear for 5 sec
+//after 5 sec tiles covered and timer starts
+function startGame () {
+
+}
+
+//creates pop up when game is completed showing user score
+function displayScore () {
+
+}
+
+//counts up all incorrect matches attempted
+function incorrectScoreTally () {
+
+}
+
+//resets game and shuffles tiles
+function resetGame () {
+
+}
+
+//turns off tile switching functionality for incorrect matches made
+function switchOff () {
+
+}
+
